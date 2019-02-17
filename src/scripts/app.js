7572bfece6298925edc9ce72a47ce6eb5f92a09c
Fernando M. Dalpiaz <fmdalpiaz@gmail.com>
2019-02-17 01:49:42 -0300

    3 slides of Idiotas

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -11,12 +11,11 @@ $(document).ready(function ()  {
 });
 
 FIC.Home = {
-    $doc: $(document),
 
     datasheetHandler: function ()  {
         var $datasheet = $('.datasheet');
 
-        FIC.Home.$doc.on('click touchend', '.datasheet-link', function () {
+        $(document).on('click', '.datasheet-link', function () {
             $datasheet
                 .addClass('open')
                 .one('touchend click', function () {
@@ -27,5 +26,50 @@ FIC.Home = {
 
     init: function () {
         FIC.Home.datasheetHandler();
+    }
+};
+
+FIC.Slides = {
+    currentSlide: 1,
+    totalSlides: 0,
+    $slides: undefined,
+
+    showHideSlides: function (prev, current) {
+        FIC.Slides.$slides.filter('.slide--' + prev + ', .slide--' + current).toggleClass('hide');
+    },
+
+    controlSlidesHandler: function () {
+        $(document).on('click', '.slide__control--prev', function () {
+            var prev = FIC.Slides.currentSlide;
+
+            if (FIC.Slides.currentSlide === 1) {
+                FIC.Slides.currentSlide = FIC.Slides.totalSlides;
+            } else {
+                FIC.Slides.currentSlide -= 1;
+            }
+
+            FIC.Slides.showHideSlides(prev, FIC.Slides.currentSlide);
+        });
+
+        $(document).on('click', '.slide__control--next', function () {
+            var prev = FIC.Slides.currentSlide;
+
+            if (FIC.Slides.currentSlide === FIC.Slides.totalSlides) {
+                FIC.Slides.currentSlide = 1;
+            } else {
+                FIC.Slides.currentSlide += 1;
+            }
+
+            FIC.Slides.showHideSlides(prev, FIC.Slides.currentSlide);
+        });
+    },
+
+    init: function () {
+        FIC.Slides.$slides = $('.slide');
+        FIC.Slides.totalSlides = FIC.Slides.$slides.length;
+
+        if (FIC.Slides.totalSlides > 0) {
+            FIC.Slides.controlSlidesHandler();
+        }
     }
 };
